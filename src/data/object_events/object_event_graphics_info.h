b42b5ba31c406842e2b27301fb4ef21a32c794fe
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-01-07 23:40:17 +0100

    Cellapolis Sud

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -2204,7 +2204,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_CableCar = {
 
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Scientist2 = {
     .tileTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_1,
+    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_4,
     .reflectionPaletteTag = OBJ_EVENT_PAL_TAG_NONE,
     .size = 256,
     .width = 32,
